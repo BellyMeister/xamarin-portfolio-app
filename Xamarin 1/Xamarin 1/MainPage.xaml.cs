7f11f8f413c16a9a34ae1b39253c7d31d3903bbb
Seppi hr. Chef <SHDa.skp@edu.mercantec.dk>
2021-01-07 11:27:00 +0100

    :))

---
+++ Xamarin 1/Xamarin 1/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xamarin_1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void ToPortfolio(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PortfolioPage());
+        }
+    }
+}
---
+++ Xamarin 1/Xamarin 1/ProjectDetails.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using static Xamarin_1.PortfolioPage;
+
+namespace Xamarin_1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProjectDetails : ContentPage
+    {
+        public ProjectDetails(Project project)
+        {
+            InitializeComponent();
+            this.Title = project.Title;
+            listView.ItemsSource = new List<Project> { project };
+        }
+    }
+}
